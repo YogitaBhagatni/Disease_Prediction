51d2020f749a36e5f76c95feb0c8a4bbb6adaa87
Yogita Bhagtani <123232581+YogitaBhagatni@users.noreply.github.com>
2024-11-30 09:55:27 +0530

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -145,7 +145,7 @@ if selected=="Projects":
           ca = st.text_input('Major vessels colored by flourosopy')
   
       with col1:
-          thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+          thal = st.selectbox("pick one"['thal: 0 = normal; 1 = fixed defect; 2 = reversable defect'])
   
       # code for Prediction
       heart_diagnosis = ''
